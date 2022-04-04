0c7849cea3c308c0a05047212dae0c4778a60d92
JuanDiegoSW <juan.lazaro.m@tecsup.edu.pe>
2022-04-03 23:07:24 -0500

    push and request

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab03
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            String servidor = txtServidor.Text;
+            String bd = txtBaseDatos.Text;
+            String user = txtUsuario.Text;
+            String pwd = txtPassword.Text;
+
+            String str = "Server=" + servidor + ";DataBase=" + bd + ";";
+
+            if (chkAutenticacion.Checked)
+                str += "Integrated Security=true";
+            else
+                str += "User Id=" + user + ";Password=" + pwd + ";";
+            try
+            {
+                conn = new SqlConnection(str);
+                conn.Open();
+                MessageBox.Show("Conexion Exitosa");
+                btnDesconectar.Enabled = true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error al conectarse el servidor: " + ex.ToString());
+            }
+        }
+
+        private void btnUsuarios_Click(object sender, EventArgs e)
+        {
+            frm_usuarios usuario = new frm_usuarios(conn);
+            usuario.Show();
+
+        }
+    }
+}
---
+++ frm_usuarios.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab03
+{
+    public partial class frm_usuarios : Form
+    {
+        SqlConnection conn;
+        public frm_usuarios(SqlConnection conn)
+        {
+            this.conn = conn;
+            InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            if( conn.State == ConnectionState.Open)
+            {
+                String sql = "select *  from tbl_Usuario";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dgvListado.DataSource = dt;
+                dgvListado.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Conexion Cerrada");
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            String aux = txtNombre.Text;
+            if (conn.State == ConnectionState.Open)
+            {
+                SqlDataAdapter datos = new SqlDataAdapter("select * from tbl_Usuario where usuario_nombre like '%" + this.txtNombre.Text + "%'",conn);
+                DataSet dt = new DataSet();
+                datos.Fill(dt, "Usuario");
+                this.dgvListado.DataSource = dt.Tables[0];
+            }
+            else
+            {
+                MessageBox.Show("Conexion Cerrada");
+            }
+        }
+    }
+}
