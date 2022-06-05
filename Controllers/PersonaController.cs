5593ff2f747b7f4a2e037524f7480930b0a798c6
JuanDiegoSW <juan.lazaro.m@tecsup.edu.pe>
2022-06-05 17:19:57 -0500

    laboratorio completado con razor

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab12.Models;
+
+namespace Lab12.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Listar()
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaID = 1,
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Direccion = "Av. Evergren 123",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@gmail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 2,
+                Nombre = "Maria",
+                Apellido = "Salas",
+                Direccion = "Av. Evergren 123",
+                FechaNac = Convert.ToDateTime("1995-11-07"),
+                Email = "maria@gmail.com"
+            });
+
+            personas.Add(new Persona
+            {
+                PersonaID = 3,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                Direccion = "Av. Evergren 123",
+                FechaNac = Convert.ToDateTime("1982-11-07"),
+                Email = "carlos@gmail.com"
+            });
+            return View(personas);
+        }
+    }
+}
